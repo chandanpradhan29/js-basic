37478f669ea011fb41ec8fd1f9d6072e28ce2a85
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-05-18 15:28:57 +0000

    variables

---
+++ 01_basic/01_variabless.js
@@ -0,0 +1,14 @@
+const accountId = 144553 ;
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState; //undefine variable : variable not assigned with values.
+
+
+// accountId =2 // not allowed
+accountEmail = "chandan@gmail.com"
+accountPassword ="cp@99"
+accountCity ="bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
