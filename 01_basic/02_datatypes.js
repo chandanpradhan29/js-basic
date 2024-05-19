396ab2b877ce2fcccf640ea108c80e8e1256b6e3
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-05-19 07:36:36 +0000

    data types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all the js as new version
+// alert(3+3) //error :we are using node js nott browser 
+
+/* 
+primitive data types :
+1. String  eg. let a="cp";
+2.number   eg let b=10
+3.boolean  eg let ans=true
+4.null     eg let c =null;
+5.undefine eg let d ;
+*/
+
+/*
+object data type :
+1 . Array
+2. object 
+3.function 
+4.date 
+*/
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefine
