2e2fd4e9b43883be9f54e0e3f29b3e4139373307
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-05-24 03:02:25 +0000

    types of data types

---
+++ 01_basic/05_datatypes_summary.js
@@ -0,0 +1,11 @@
+/*
+How data are being stored in memory and how they are accessing based of this it categorized into two types 
+1) primitive data type
+2)non primitive data type
+
+q) interview question : 
+js is dynamically typed language or statically typed language ???
+
+
+*/
+
