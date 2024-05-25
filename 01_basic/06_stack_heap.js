80dfcba017e6cf3b678b7adb7a08f9da62bdd60c
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-05-25 02:55:38 +0000

    stack and heap

---
+++ 01_basic/06_stack_heap.js
@@ -0,0 +1,21 @@
+//stack(primitive data type)
+let ytName = "chandanpradhan"
+let anothername = ytName;
+anothername = "chaiaursuta"
+
+console.log(ytName);
+console.log(anothername);
+
+//heap(non primtive data type)
+let user1 = {
+    name : "chandan" ,
+    up :"ybl@gmail.com"
+}
+
+let user2 = user1;
+user2.name ="chiku";
+
+console.log(user1.name);
+console.log(user2.name);
+
+
