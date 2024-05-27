1e13666c1a8b3a26d4b3341bada504109d364f42
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-05-27 03:09:33 +0000

    array2 basic

---
+++ 02_basics/02_array.js
@@ -0,0 +1,12 @@
+const arr = [1 , 2 , 3 , [4,5,6] , 7 , [6 , 7 , [4,5]]];
+const result_arr = arr.flat(Infinity);
+// console.log(result_arr);
+
+const myarr2 = [12,13,"cp"];
+console.log(Array.isArray([1,2,3])); //false
+
+const name_arr = Array.from("chandan"); // convert names pass on paramater as array like ['c' ,'h' , 'a' , 'n' , 'd' , 'a' , 'n']
+console.log(name_arr);
+
+//from()
+//of()
