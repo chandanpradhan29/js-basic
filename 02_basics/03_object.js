839c3dfaee3d0c5ab3f5e4d5e80ad7f90ec097b9
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-05-30 02:28:42 +0000

    objects1

---
+++ 02_basics/03_object.js
@@ -0,0 +1,11 @@
+const myobj = {
+    name : "chandan" ,
+    age : 25 ,
+    city : "banglore" ,
+    isLoggedin : false ,
+    loginDays : ["monday" , "tuesday"]
+
+}
+
+console.log(myobj.name);
+console.log(myobj["name"]);
