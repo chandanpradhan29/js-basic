89e38e4cc9dac00d4a00b57898bacac2f83e0312
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-05-30 15:06:51 +0000

    object2

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,11 +1,24 @@
+const mysymb = Symbol("113")
 const myobj = {
     name : "chandan" ,
+    "full name" : "chandan kumar" , //can't access this using .dot operator
     age : 25 ,
     city : "banglore" ,
     isLoggedin : false ,
-    loginDays : ["monday" , "tuesday"]
-
+    loginDays : ["monday" , "tuesday"] ,
+    [mysymb] : "key1"
 }
 
 console.log(myobj.name);
 console.log(myobj["name"]);
+//console.log(myobj.full name);//error
+//accessing symbol :
+console.log(myobj[mysymb]);
+
+//update properties 
+myobj.name ="raaj naam tho suna hoga"
+
+//freez object : we can't do any updation once object is freez 
+Object.freeze(myobj)
+myobj.name ="simran naam tho suna hoga"
+console.log(myobj);
