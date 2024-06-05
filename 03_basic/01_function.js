be8ef13a97cf7ab0274944b907f9e9008cb5b768
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-06-05 03:21:12 +0000

    function part1

---
+++ 03_basic/01_function.js
@@ -0,0 +1,28 @@
+/*
+function addTwoNums(number1 , number2) {
+    console.log(number1+number2);
+}
+//function call 
+addTwoNums(9,7);
+*/
+
+function addTwoNums(number1 , number2) {
+    // let  result = number1 + number2
+    // return result
+    return number1+number2
+    
+}
+
+function loginUserMsg(username="chandu"){
+    if(!username){ //equal to : username == undefined
+        console.log("please enter username");
+        return
+    }
+    return `${username} is logged in`
+}
+//function call 
+console.log(loginUserMsg("chandanpradhan"));
+console.log(loginUserMsg("rakul"));//undefine is logged in (when we not passsed any value)----> interview question 
+
+// const sum = addTwoNums(9,7);
+// console.log("sum is : ",sum);
