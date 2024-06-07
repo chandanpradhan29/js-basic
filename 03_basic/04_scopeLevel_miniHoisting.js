a9ab3067a89026b885a29cfe5f928aafc66d7f79
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-06-07 02:57:01 +0000

    hositing_scopelevel

---
+++ 03_basic/04_scopeLevel_miniHoisting.js
@@ -0,0 +1,39 @@
+function one(){
+    const username = "hitesh" 
+
+    function two() {
+        const website ="youtube"
+        console.log("function two() executed : " , website);
+    }
+    // console.log(website)//error : aceesing website outside the scope
+
+    two()
+}
+
+// one();
+
+//if else 
+
+if(true) {
+    const username = "chandan"
+
+    if(true){
+        const website = "youtube"
+        // console.log(website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// hoisting 
+
+console.log(addTwoNos(10,5));
+function addTwoNos(num1 , num2) {
+    return num1+num2
+}
+
+console.log(sum(10,2));
+const sum = function(num1,num2) {
+    return num1+num2
+}
