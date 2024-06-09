aaf2881fa78e2257789327ee7b2b2dd744b428bc
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-06-09 12:21:51 +0000

    arrow this

---
+++ 03_basic/this_arrow.js
@@ -0,0 +1,48 @@
+//this keyword will work on for object properties .
+// this keyword will not work for function proprties.
+const user = {
+    usename : "chandan" ,
+    location : "banglore"
+}
+
+const f1 = function() {
+    console.log(`username is ${user.usename}`);
+    console.log(user.usename);
+    console.log(this);
+}
+
+// console.log(user.usename);
+// f1();
+// console.log(this);
+
+
+const f2 = function(){
+    const name ="harish"
+    const age = 24 
+
+    console.log(this.age);//undefine
+
+}
+// f2();
+
+//#####################this end here####################
+
+//###################### arrow function #################
+
+const addTwoNums = function(num1,num2) {
+    return num1+num2
+}
+
+// console.log(addTwoNums(10,6));
+
+const numAvg = (num)=> {
+  return num/2
+}
+
+// console.log(numAvg(10));
+
+const numAvg1 = (num)=>  (num/2)
+
+console.log(numAvg1(10));
+
+
