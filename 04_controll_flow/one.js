fd40fb285490c81c0fd13c41566ead67978d83a0
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-06-16 02:58:59 +0000

    if commit

---
+++ 04_controll_flow/one.js
@@ -0,0 +1,17 @@
+let balance =200;
+if(balance < 100) {
+    console.log("blanace is less than 100");
+}
+else if(balance < 300) {
+    console.log("blanace is less than 300");
+}
+else if(balance < 700) {
+    console.log("blanace is less than 700");
+}
+else {
+    console.log(`balance is ${balance} `);
+}
+
+// || -> return true if any one condition is satisy the condition 
+// && -> return true is both condition satisy the condition 
+
