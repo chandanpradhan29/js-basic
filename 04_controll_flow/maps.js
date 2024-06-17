c46e2ba56f3311b417afb3c32c14487db4066bb5
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-06-17 08:12:11 +0000

    last topic nested map filter

--- 04_controll_flow/maps.js
+++ 04_controll_flow/maps.js
@@ -1,5 +1,13 @@
 //adding 10 to every elemts
-const nums = [1,2,3,4,5,6,7,8,8,10]
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const result = nums.map ( (val)=> val+10)
-console.log(result);
+const result = nums.map((val) => val + 10)
+// console.log(result);
+
+//nesting map and filter
+
+const resultNums = nums
+                  .map( (num) => num * 10)
+                  .map( (num) => num + 1)
+                  .filter( (num)=> num>=40 )
+console.log(resultNums);
