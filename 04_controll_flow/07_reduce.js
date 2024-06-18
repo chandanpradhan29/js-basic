a590a3b08dec252d2a2c4ce37d35e16ee343dac2
chandanpradhan29 <168625174+chandanpradhan29@users.noreply.github.com>
2024-06-18 03:21:53 +0000

    reduce topic last cover

---
+++ 04_controll_flow/07_reduce.js
@@ -0,0 +1,37 @@
+const nums = [1,2,3]
+// const total = nums.reduce( function(acc , currVal) {
+//     console.log(`acc : ${acc} and currVal : ${currVal} `);
+//     return acc + currVal
+// } , 0 );
+
+//using arrow function 
+const total = nums.reduce( (acc,currVal) => (acc + currVal) , 0 )
+// console.log(total);
+
+//course cart 
+
+const courses = [
+    {
+        item : "js course" ,
+        price : 2999
+    } ,
+
+    {
+        item : "dev course" ,
+        price : 999
+    } ,
+
+    {
+        item : "react course" ,
+        price : 3999
+    } ,
+
+    {
+        item : "app dev course" ,
+        price : 5999
+    } ,
+
+]
+
+const toatlCartPrice = courses.reduce ( (acc ,item) => (acc + item.price), 0 )
+console.log(toatlCartPrice);
